0f621cc00428ffa1be435885ebb43a556f8370bc
mykomaus <mykoasot@gmail.com>
2023-09-17 11:56:50 +0200

    refactor: file

--- lesson1/src/script.js
+++ lesson1/src/script.js
@@ -1,153 +1,41 @@
-const domContainer = document.querySelector('#root');
+const domContainer = document.querySelector("#root");
 const root = ReactDOM.createRoot(domContainer);
-
-const CARS = [
-    {
-        id: 1,
-        brand: "Audi",
-        models: [
-            {
-                id: 1,
-                name: "A1",
-                collection: [
-                    {
-                        id: 1,
-                        version: "Sportback",
-                        year: 2019,
-                        horsepower: 95,
-                        engine: 999
-                    },
-                    {
-                        id: 2,
-                        version: "Citycarver",
-                        year: 2019,
-                        horsepower: 95,
-                        engine: 999
-                    }
-                ]
-            },
-            {
-                id: 2,
-                name: "Q5",
-                collection: [
-                    {
-                        id: 1,
-                        version: "FY 2021",
-                        year: 2021,
-                        horsepower: 299,
-                        engine: 1984
-                    },
-                    {
-                        id: 2,
-                        version: "Sportback",
-                        year: 2021,
-                        horsepower: 299,
-                        engine: 1984
-                    }
-                ]
-            },
-            {
-                id: 3,
-                name: "TT",
-                collection: [
-                    {
-                        id: 1,
-                        version: "Coupe",
-                        year: 2021,
-                        horsepower: 197,
-                        engine: 1984
-                    },
-                    {
-                        id: 2,
-                        version: "Roadster",
-                        year: 2021,
-                        horsepower: 197,
-                        engine: 1984
-                    }
-                ]
-            }
-        ]
-    },
-    {
-        id: 2,
-        brand: "BMW",
-        models: [
-            {
-                id: 1,
-                name: "8 series",
-                collection: [
-                    {
-                        id: 1,
-                        version: "G1X LCI",
-                        year: 2022,
-                        horsepower: 333,
-                        engine: 2998
-                    },
-                    {
-                        id: 2,
-                        version: "G1X",
-                        year: 2019,
-                        horsepower: 340,
-                        engine: 2998
-                    }
-                ]
-            },
-            {
-                id: 2,
-                name: "X6",
-                collection: [
-                    {
-                        id: 1,
-                        version: "G06 LCI",
-                        year: 2023,
-                        horsepower: 530,
-                        engine: 4395
-                    },
-                    {
-                        id: 2,
-                        version: "G06",
-                        year: 2020,
-                        horsepower: 286,
-                        engine: 2993
-                    }
-                ]
-            }
-        ]
-    },
-];
+import { CARS } from "./cars";
 
 root.render(
-<React.Fragment>
-   <table>
-   <tbody>
-    {CARS.map((item, index) =>
-    <tr key={index}>
-        <tr key={index}>
-            <td colSpan="2" className="red_background">
-              {item.brand}
-            </td>
-        </tr>
-        {item.models.map((itemM) => 
-                itemM.collection.map((item, index, array) => 
-                    <tr key={index}>
-                    {index == 0 ? <td rowSpan={array.length} className="yellow_background">{itemM.name}</td> : null}
-                    <td>
-                        <ul>
-                        {Object.keys(item)
-                        .filter(key => key!== 'id')
-                        .map((key) => 
-                            <li key={key}>
-                                {key + ": " + item[key]}
-                             </li>)}
-                        </ul>
+  <React.Fragment>
+    <table>
+      <tbody>
+        {CARS.map((item, index) => (
+          <tr key={index}>
+            <tr key={index}>
+              <td colSpan="2" className="red_background">
+                {item.brand}
+              </td>
+            </tr>
+            {item.models.map((itemM) =>
+              itemM.collection.map((item, index, array) => (
+                <tr key={index}>
+                  {index == 0 ? (
+                    <td rowSpan={array.length} className="yellow_background">
+                      {itemM.name}
                     </td>
-                    </tr>
-                    
-                ) 
+                  ) : null}
+                  <td>
+                    <ul>
+                      {Object.keys(item)
+                        .filter((key) => key !== "id")
+                        .map((key) => (
+                          <li key={key}>{key + ": " + item[key]}</li>
+                        ))}
+                    </ul>
+                  </td>
+                </tr>
+              ))
             )}
-    </tr>
-    )}
-        </tbody>
-   </table>
-</React.Fragment>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </React.Fragment>
 );
